d47011b57b475661ca2423640baa14313e9fd7c2
AlanSFSU <ang3@mail.sfsu.edu>
2020-10-09 00:53:55 -0700

    added template and css work from yesterday

---
+++ static/index.js
@@ -0,0 +1,11 @@
+function toggleText() {
+  var x = document.getElementById("myText");
+
+  if(x.style.display == "none")
+  {
+    x.style.display = "block";
+  }
+  else{
+    x.style.display="none";
+  }
+}
