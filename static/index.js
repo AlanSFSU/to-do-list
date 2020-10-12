a18b2ea7ac3dc600439165a9a4b0d0024543dcea
AlanSFSU <ang3@mail.sfsu.edu>
2020-10-12 02:07:14 -0700

    created an add button and appends items onto the list

--- static/index.js
+++ static/index.js
@@ -1,11 +1,29 @@
-function toggleText() {
-  var x = document.getElementById("myText");
+function newElement()
+{
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("newInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
 
-  if(x.style.display == "none")
+  if(inputValue === '')
   {
-    x.style.display = "block";
+    alert("you must write something!");
   }
   else{
-    x.style.display="none";
+    document.getElementById("list").appendChild(li);
   }
+  document.getElementById("myInput").value = "";
+
+var span = document.createElement("SPAN");
+var txt = document.createTextNode("\u00D7");
+span.className = "close";
+span.appendChild(txt);
+li.appendChild(span);
+
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
 }
